3ec9c9826cfa2305de157d80a32487f92c9376ce
Roy <royandroll@outlook.com>
2022-02-24 21:07:01 +0800

    1

--- src/components/Pay.tsx
+++ src/components/Pay.tsx
@@ -6,8 +6,6 @@ import styles from '../assets/styles/Name.module.scss'
 // import { useAuthWallet } from "../context/AuthWallet";
 // import { toast } from 'react-toastify';
 import ServiceApi from "../utils/ServiceApi";
-import {IC_NAMING_LEDGER_ID} from "../utils/canisters/icNamingLedger/canisterId";
-import {createLedgerActor} from "../utils/canisters/ledger";
 import {BlockIndex, Tokens} from "../utils/canisters/ledger/interface";
 declare const window: any;
 
@@ -25,7 +23,6 @@ export const Pay: React.FC<PayPorps> = ({ regname, payType, payYears, payQuota, 
   const [loadingSubmit, setLoadingSubmit] = useState<boolean>(false);
   const [hasRefund, setHasRefund] = useState<boolean>(false)
   const serviceApi = new ServiceApi();
-  const ledgerActor = createLedgerActor();
  /*  useEffect(() => {
     console.log(regname)
     console.log(payType)
@@ -73,48 +70,34 @@ export const Pay: React.FC<PayPorps> = ({ regname, payType, payYears, payQuota, 
       return
     }
     let order = orderResult[0];
-    const transfer_result = await ledgerActor.transfer({
-      to:order.payment_account_id,
-      memo:order.payment_memo.ICP,
-      fee:{
-        e8s:BigInt(10000)
-      },
-      amount:{
-        e8s:order.price_icp_in_e8s
-      },
-      created_at_time:[],
-      from_subaccount:[]
-    });
+    const arrayToHex = (arr: Array<number>) => {
+      return arr.map(item => {
+        return item.toString(16)
+      }).join('')
+    }
 
-    if ('Ok' in transfer_result){
-      toast.success('Payment is successful, please wait for seconds, the system will confirm your payment, please do not repeat during this period',{
-        position:'top-center',
-        theme:'dark'
+    try {
+      const result = await window.ic.requestTransfer({
+        to: arrayToHex(order.payment_account_id),
+        amount: order.price_icp_in_e8s,
+        opts: {
+          fee: 10000,
+          memo: order.payment_memo.ICP,
+        },
+      });
+
+      toast.success('Payment is successful, please wait for seconds, the system will confirm your payment, please do not repeat during this period', {
+        position: 'top-center',
+        theme: 'dark'
       })
-    }else {
-      let errorMessage = '';
-      // create error message according to Err type
-      let err = transfer_result.Err;
-      if ('TxTooOld' in err) {
-        errorMessage = 'Transaction is too old';
-      } else if ('BadFee' in err) {
-        errorMessage = 'Bad fee';
-      } else if ('TxDuplicate' in err) {
-        errorMessage = 'Transaction is duplicate';
-      } else if ('TxCreatedInFuture' in err) {
-        errorMessage = 'Transaction is created in future';
-      } else if ('InsufficientFunds' in err) {
-        errorMessage = 'Insufficient funds';
-      } else {
-        errorMessage = 'Unknown error';
-      }
-
-      toast.error(`fail transfer with error: ${errorMessage}`, {
+    } catch (err) {
+      toast.error(`fail transfer with error: ${err}`, {
         position: 'top-center',
         theme: 'dark'
       })
       return
     }
+
   }
 
   const cancelRegisterOrder = () => {
