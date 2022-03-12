a8fe85c5bfc98b66ced812cd283c8c9e0cce343b
Kevin-icnaming <fxg971123@gmail.com>
2022-03-12 21:59:22 +0800

    Improve:catch toast err

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -50,6 +50,8 @@ export const Card: React.FC<CardProps> = ({ name, expireAt, available, isMyAccou
         deleteCache('myNamesOfFavorite' + auth.walletAddress);
         console.log("addFavorite", res)
       }
+    }).catch(err=>{
+      toast.error(err.message)
     })
   }
 
